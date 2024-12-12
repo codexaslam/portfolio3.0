ab016d81c7d18210784c4b1b1d452dccef07dcbf
Aslam Hossain <aslamhossain@Aslams-MacBook-Air.local>
2024-12-12 17:40:51 +0200

    new portfolio is ready for hosting

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,88 +7,80 @@ import { Card, CardContent } from "@/components/ui/card";
 import { JobImages } from "@/components/JobImages";
 
 const jobs = [
-    {
-        role: "Senior Full-Stack Developer", // TODO: Replace with actual role
-        company: "Tech Innovators Inc.", // TODO: Replace with actual company name
-        logo: "/company.svg", // TODO: Replace with actual logo
-        duration: "2020 - Present",
-        description:
-            "Lead developer for multiple high-impact projects, mentoring junior developers, and implementing best practices.",
-        link: "https://techinnovators.com",
-        images: [],
-    },
-    {
-        role: "Senior Full-Stack Developer", // TODO: Replace with actual role
-        company: "Tech Innovators Inc.", // TODO: Replace with actual company name
-        logo: "/company.svg", // TODO: Replace with actual logo
-        duration: "2018 - 2020",
-        description:
-            "Developed and maintained various client projects, focusing on responsive design and performance optimization.",
-        link: "https://websolutions.com",
-        images: [
-            "/devvault.png",
-            "/devvault.png",
-        ],
-    },
-    {
-        role: "Senior Full-Stack Developer", // TODO: Replace with actual role
-        company: "Tech Innovators Inc.", // TODO: Replace with actual company name
-        logo: "/company.svg", // TODO: Replace with actual logo
-        duration: "2016 - 2018",
-        description:
-            "Assisted in the development of web applications, gained experience in agile methodologies and version control.",
-        link: "https://startupventures.com",
-        images: [
-            "/devvault.png",
-            "/devvault.png",
-        ],
-    },
-]
+  {
+    role: "Software Engineer", // TODO: Replace with actual role
+    company: "SELISE Digital Platforms", // TODO: Replace with actual company name
+    logo: "/selise.png", // TODO: Replace with actual logo
+    duration: "2021 - August 2024",
+    description: [
+      "Led a web app and a mobile app team to success, guiding 5 junior developers to enhance their skills in full-stack and 2 developers in Flutter and stay current with emerging technologies.",
+      "Architected a highly scalable website that seamlessly accommodates over 500,000 monthly users with zero downtime.",
+      "Designed and launched a cutting-edge mobile app featuring QR scanning, real-time socket updates, and geo-based push notifications, achieving 30,000+ downloads in 30 days.",
+    ],
+    link: "https://selisegroup.com/",
+    images: ["/selise_ex.png"],
+  },
+  {
+    role: "Frontend Engineer", // TODO: Replace with actual role
+    company: "Crantech LLC", // TODO: Replace with actual company name
+    logo: "/crantech.svg", // TODO: Replace with actual logo
+    duration: "2020 - 2021 April",
+    description: [
+      "Engineered real estate and medical web applications, supporting over 70,000 live users, and improving client business performance by 24% through enhanced user experiences and scalability.",
+      "Designed a cross-platform audio meeting app that accelerated company communication by 10x, enabling real-time, seamless interactions across devices.",
+      "Constructed a social medical app for online doctor consultations, digital prescriptions appointment booking, and remote patient management.",
+    ],
+    link: "https://crantechllc.com/",
+    images: ["/crantech_ex.png"],
+  },
+];
 
 export const Experience = () => {
-    return (
-        <>
-            <h2 className="text-xl font-bold mb-4">Work Experience</h2>
-            <Card>
-                <CardContent className="pt-6">
-                    <ul className="space-y-8">
-                        {jobs.map((j, i) => (
-                            <li key={i} className="border-b last:border-b-0 pb-8 last:pb-0">
-                                {/* Job Details */}
-                                <div className="flex items-center space-x-4">
-                                    <Image
-                                        src={j.logo}
-                                        alt={j.company}
-                                        width={40}
-                                        height={40}
-                                        className="rounded-md border shadow-md object-cover"
-                                    />
-                                    <div>
-                                        <h3 className="font-semibold">
-                                            {j.role}
-                                        </h3>
-                                        <p className="text-sm text-muted-foreground">
-                                            {j.company}
-                                        </p>
-                                    </div>
-                                </div>
-                                <p className="text-xs text-muted-foreground mt-2 flex items-center">
-                                    <CalendarDays className="size-3 mr-2"/>
-                                    {j.duration}
-                                </p>
-                                <p className="text-sm mt-2">{j.description}</p>
-                                {/* Job Images */}
-                                <JobImages 
-                                    role={j.role} 
-                                    link={j.link}
-                                    images={j.images} 
-                                    duration={j.duration} 
-                                />
-                            </li>
-                        ))}
-                    </ul>
-                </CardContent>
-            </Card>
-        </>
-    )
-}
+  return (
+    <>
+      <h2 className="text-xl font-bold mb-4">Work Experience</h2>
+      <Card>
+        <CardContent className="pt-6">
+          <ul className="space-y-8">
+            {jobs.map((j, i) => (
+              <li key={i} className="border-b last:border-b-0 pb-8 last:pb-0">
+                {/* Job Details */}
+                <div className="flex items-center space-x-4">
+                  <Image
+                    src={j.logo}
+                    alt={j.company}
+                    width={80}
+                    height={40}
+                    className="rounded-md object-cover"
+                  />
+                  <div>
+                    <h3 className="font-semibold">{j.role}</h3>
+                    <p className="text-sm text-muted-foreground">{j.company}</p>
+                  </div>
+                </div>
+                <p className="text-xs text-muted-foreground mt-2 flex items-center">
+                  <CalendarDays className="size-3 mr-2" />
+                  {j.duration}
+                </p>
+                <ul className="list-disc pl-5 space-y-2">
+                  {j.description?.map((line, indx) => (
+                    <li key={indx} className="!m-0">
+                      <p className="text-sm">{line}</p>
+                    </li>
+                  ))}
+                </ul>
+                {/* Job Images */}
+                <JobImages
+                  role={j.role}
+                  link={j.link}
+                  images={j.images}
+                  duration={j.duration}
+                />
+              </li>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,28 +1,49 @@
-import {
-    Card,
-    CardTitle,
-    CardHeader,
-    CardContent,
-} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-const skills = ["TypeScript", "JavaScript", "Python", "Java", "C#", "C++", "C", "SQL", "HTML", "CSS", "TailwindCSS", "NextJS", "ReactJS", "NodeJS", "ExpressJS", "MongoDB", "PostgreSQL", "Git", "Docker", "Kubernetes", "Linux", "Windows", "MacOS"]
+const skills = [
+  "TypeScript",
+  "JavaScript",
+  "ReactJS",
+  "NextJS",
+  "ReduxToolkit",
+  "ReactQuery",
+  "TailwindCSS",
+  "ReactQuery",
+  "MaterialUI",
+  "SASS",
+  "Flutter",
+  "Firebase",
+  "SQLite",
+  "WebSockets",
+  "NodeJS",
+  "ExpressJS",
+  "MongoDB",
+  "MySQL",
+  "Redis",
+  "Storyblok",
+  "Git",
+  "AWS",
+  "Azure",
+  "Docker",
+  "Kubernetes",
+];
 
 export const Skills = () => {
-    return (
-        <Card className="mt-6">
-            <CardHeader>
-                <CardTitle>Skills</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <div className="flex flex-wrap gap-2">
-                    {skills.map((s, i) => (
-                        <Badge key={i} variant="secondary">
-                            {s}
-                        </Badge>
-                    ))}
-                </div>
-            </CardContent>
-        </Card>
-    )
-}
+  return (
+    <Card className="mt-6">
+      <CardHeader>
+        <CardTitle>Skills</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-wrap gap-2">
+          {skills.map((s, i) => (
+            <Badge key={i} variant="secondary">
+              {s}
+            </Badge>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
